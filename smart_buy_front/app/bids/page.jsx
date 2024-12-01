99723e1f9382f768c595c759036b0553e1e09e87
Luiz Cunha <felipe-f-cunha@hotmail.com>
2024-12-01 11:31:58 -0300

    feat: Implementado tela de aletas concluidos

---
+++ smart_buy_front/app/bids/page.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import AlertCard from "../_components/alertCard";
+import { redirect } from "next/navigation";
+import { useAlerts } from "../_hooks/useAlerts";
+import { formatDate, handleStatus } from "../_utils";
+import Spinner from "../_components/spinner";
+import { useAuth } from "../_hooks/useAuth";
+import { useBids } from "../_hooks/useBids";
+
+export default function Home() {
+  if (!localStorage.getItem("token")) {
+    redirect("/");
+  }
+
+  const { bids, loading } = useBids();
+
+  const handleClick = (id, phone) => {
+    redirect(`/alerts/${id}?is_winner=true&phone=${phone}`);
+  };
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold mb-10 p-10 lg:p-0">
+        Alertas concluidos
+      </h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="flex flex-col items-center gap-20 md:flex-row md:justify-center lg:flex-wrap lg:justify-start">
+          {bids.length > 0 ? (
+            bids?.map((bid, index) => (
+              <AlertCard
+                title={bid.product_name}
+                onClick={() => handleClick(bid.alert_id, bid.telefone)}
+                key={index}
+              >
+                <ul>
+                  <li>Quantidade: {bid.quantity_needed}</li>
+                  <li>
+                    Prazo da negociação: {formatDate(bid.negotiation_deadline)}
+                  </li>
+                  <li className="capitalize">
+                    Status: {handleStatus(bid.status)}
+                  </li>
+                </ul>
+              </AlertCard>
+            ))
+          ) : (
+            <div className="flex items-center justify-center h-screen w-full">
+              <h2 className="text-center text-2xl font-bold">
+                Ainda não existem alertas criados!
+              </h2>
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
+}
