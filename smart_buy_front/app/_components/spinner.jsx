4ed0cd0dc8c607bf70169b56d1ed9e8e5b459bbc
Luiz Cunha <felipe-f-cunha@hotmail.com>
2024-12-01 11:36:04 -0300

    feat: implementado spinner

--- smart_buy_front/app/_components/spinner.jsx
+++ smart_buy_front/app/_components/spinner.jsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
   return (
     <div className="flex items-center justify-center">
-      <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
+      <div className="w-20 h-20 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
     </div>
   );
 }
