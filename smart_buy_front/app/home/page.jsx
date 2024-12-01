4c9a35589dedaaa1a89663069bf98d193332e99a
Luiz Cunha <felipe-f-cunha@hotmail.com>
2024-12-01 11:31:35 -0300

    feat: implementado socket e adicionado verificação de alertas

--- smart_buy_front/app/home/page.jsx
+++ smart_buy_front/app/home/page.jsx
@@ -1,16 +1,47 @@
 "use client";
 
-import AlertCard from "../_components/alertCard";
 import { redirect } from "next/navigation";
 import { useAlerts } from "../_hooks/useAlerts";
-import { formatDate, handleStatus } from "../_utils";
+import Spinner from "../_components/spinner";
+import { useAuth } from "../_hooks/useAuth";
+import AlertList from "../_components/alertList";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+import { emitToast } from "../_utils";
 
 export default function Home() {
   if (!localStorage.getItem("token")) {
     redirect("/");
   }
 
-  const { alerts } = useAlerts();
+  const { alerts, loading, refreshAlerts } = useAlerts();
+  const { userType, userId } = useAuth();
+
+  const filteredAlerts = alerts.filter((alert) => !alert.user_id_assigned);
+  const alertsToRender = userType === "cliente" ? alerts : filteredAlerts;
+
+  useEffect(() => {
+    const socket = io("http://192.168.1.7:3001");
+
+    if (userType === "fornecedor") {
+      socket.emit("register_profile", "fornecedor");
+    }
+
+    socket.emit("register_user", userId);
+
+    socket.on("new_alert", (data) => {
+      emitToast("info", data.message);
+      refreshAlerts();
+    });
+
+    socket.on("new_bid", (data) => {
+      emitToast("info", data.message);
+    });
+
+    socket.on("bid_accepted", (data) => {
+      emitToast("info", data.message);
+    });
+  }, [userType, userId]);
 
   const handleClick = (id) => {
     redirect(`/alerts/${id}`);
@@ -18,25 +49,26 @@ export default function Home() {
 
   return (
     <>
-      <h1 className="text-4xl font-bold mb-10 p-10">Seus Alertas</h1>
-      <div className="flex flex-col items-center justify-between gap-20 lg:flex-wrap">
-        {alerts?.map((alert) => (
-          <AlertCard
-            title={alert.product_name}
-            onClick={() => handleClick(alert.alert_id)}
-          >
-            <ul>
-              <li>Quantidade: {alert.quantity_needed}</li>
-              <li>
-                Prazo da negociação: {formatDate(alert.negotiation_deadline)}
-              </li>
-              <li className="capitalize">
-                Status: {handleStatus(alert.status)}
-              </li>
-            </ul>
-          </AlertCard>
-        ))}
-      </div>
+      <h1 className="text-4xl font-bold mb-10 p-10 lg:p-0">
+        {userType === "cliente" ? "Seus Alertas" : "Alertas disponíveis"}
+      </h1>
+      {loading ? (
+        <div className="flex items-center justify-center h-screen">
+          <Spinner />
+        </div>
+      ) : (
+        <div className="flex flex-col items-center gap-20 mb-24 md:flex-row md:justify-center lg:flex-wrap lg:justify-start">
+          {alerts.length > 0 ? (
+            <AlertList alerts={alertsToRender} handleClick={handleClick} />
+          ) : (
+            <div className="flex items-center justify-center h-screen w-full">
+              <h2 className="text-center text-2xl font-bold">
+                Ainda não existem alertas criados!
+              </h2>
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 }
