f08b678576a40b0c275527e36e82445a96e7604a
Luiz Cunha <felipe-f-cunha@hotmail.com>
2024-12-01 11:29:04 -0300

    feat: implementado verificação de cnpj no signup

---
+++ smart_buy_front/api/receitaWS.js
@@ -0,0 +1,21 @@
+import $ from "jquery";
+
+export default async function consultCNPJ(cnpj) {
+  const url = `https://www.receitaws.com.br/v1/cnpj/${cnpj}`;
+  let companySituation;
+
+  await $.ajax({
+    url: url,
+    dataType: "jsonp",
+    success: function (data) {
+      console.log(data);
+
+      companySituation = data.situacao;
+    },
+    error: function (xhr, status, error) {
+      console.error({ xhr, status, error });
+    },
+  });
+
+  return companySituation;
+}
--- smart_buy_front/app/singup/page.jsx
+++ smart_buy_front/app/singup/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
@@ -12,11 +12,15 @@ import * as yup from "yup";
 import "react-toastify/dist/ReactToastify.css";
 import Modal from "../_components/modal";
 import { redirect } from "next/navigation";
+import Spinner from "../_components/spinner";
+import consultCNPJ from "@/api/receitaWS";
 
 export default function Signup() {
   const [phoneValue, setPhoneValue] = useState("");
   const [cnpjValue, setCnpjValue] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isNotActive, setIsNotActive] = useState(false);
 
   const schema = yup.object({
     username: yup.string().required("Usuário é obrigatório"),
@@ -64,13 +68,25 @@ export default function Signup() {
     const cleanCnpjNumber = cnpj.replace(/\D/g, "");
     const cleanPhoneNumber = phone.replace(/\D/g, "");
 
-    const response = await signUp({
-      ...data,
-      phone: cleanPhoneNumber,
-      cnpj: cleanCnpjNumber,
-    });
-    if (response.status === 200) {
-      setIsModalOpen(true);
+    setIsLoading(true);
+
+    const companyState = await consultCNPJ(cleanCnpjNumber);
+
+    if (companyState === "ATIVA") {
+      const response = await signUp({
+        ...data,
+        phone: cleanPhoneNumber,
+        cnpj: cleanCnpjNumber,
+      });
+
+      if (response.status === 200) {
+        setIsModalOpen(true);
+        setIsLoading(false);
+      }
+    } else {
+      setIsLoading(false)
+      setIsNotActive(true);
+      setIsModalOpen(true)
     }
   };
 
@@ -83,119 +99,122 @@ export default function Signup() {
         height={200}
         alt="Logo Smart Buy"
       />
-      <form className="flex flex-col" onSubmit={handleSubmit(submit)}>
-        <div className="flex flex-col">
-          <div className="input-group">
-            <label>Usuário</label>
-            <input
-              type="text"
-              className="default-input"
-              {...register("username", { required: true })}
-            />
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <form className="flex flex-col" onSubmit={handleSubmit(submit)}>
+          <div className="flex flex-col">
+            <div className="input-group">
+              <label>Usuário</label>
+              <input
+                type="text"
+                className="default-input"
+                {...register("username", { required: true })}
+              />
+            </div>
+            {errors.username && (
+              <small className="text-red-600">{errors.username.message}</small>
+            )}
+            <div className="input-group">
+              <label>Senha</label>
+              <input
+                className="default-input"
+                type="password"
+                {...register("password", { required: true })}
+              />
+              <p className="m-0 p-0 leading-3 font-medium italic">
+                <small>
+                  Minimo 6 digitos, deve conter letra maiúscula, numeros e
+                  caractere especial
+                </small>
+              </p>
+            </div>
+            {errors.password && (
+              <small className="text-red-600">{errors.password.message}</small>
+            )}
+            <div className="input-group">
+              <label>Nome Completo</label>
+              <input
+                className="default-input"
+                type="text"
+                {...register("name", { required: true })}
+              />
+            </div>
+            {errors.name && (
+              <small className="text-red-600">{errors.name.message}</small>
+            )}
+            <div className="input-group">
+              <label>E-mail</label>
+              <input
+                className="default-input"
+                type="email"
+                {...register("email", { required: true })}
+              />
+            </div>
+            {errors.email && (
+              <small className="text-red-600">{errors.email.message}</small>
+            )}
+            <div className="input-group">
+              <label>Endereço</label>
+              <input
+                className="default-input"
+                type="text"
+                {...register("address", { required: true })}
+              />
+            </div>
+            {errors.address && (
+              <small className="text-red-600">{errors.address.message}</small>
+            )}
+            <div className="input-group">
+              <label>Telefone</label>
+              <input
+                className="default-input"
+                type="tel"
+                maxLength={15}
+                onInput={(e) => setPhoneValue(handlePhoneChange(e))}
+                value={phoneValue}
+                {...register("phone", { required: true })}
+              />
+            </div>
+            {errors.phone && (
+              <small className="text-red-600">{errors.phone.message}</small>
+            )}
+            <div className="input-group">
+              <label>CNPJ</label>
+              <input
+                className="default-input"
+                type="text"
+                onInput={(e) => setCnpjValue(handleCnpjChange(e))}
+                value={cnpjValue}
+                maxLength={18}
+                {...register("cnpj", { required: true })}
+              />
+            </div>
+            {errors.cnpj && (
+              <small className="text-red-600">{errors.cnpj.message}</small>
+            )}
+            <div className="input-group">
+              <label>Tipo</label>
+              <select
+                className="default-input"
+                {...register("type", { required: true })}
+              >
+                <option value="cliente">Microempreendedor</option>
+                <option value="fornecedor">Fornecedor</option>
+              </select>
+            </div>
+            {errors.type && (
+              <small className="text-red-600">{errors.type.message}</small>
+            )}
           </div>
-          {errors.username && (
-            <small className="text-red-600">{errors.username.message}</small>
-          )}
-          <div className="input-group">
-            <label>Senha</label>
-            <input
-              className="default-input"
-              type="password"
-              {...register("password", { required: true })}
-            />
-            <p className="m-0 p-0 leading-3 font-medium italic">
-              <small>
-                Minimo 6 digitos, deve conter letra maiúscula, numeros e
-                caractere especial
-              </small>
-            </p>
+          <div className="flex justify-between">
+            <Link href={"/"}>
+              <button className="alt-button self-start mt-8">Voltar</button>
+            </Link>
+            <button className="self-start mt-8">Enviar</button>
           </div>
-          {errors.password && (
-            <small className="text-red-600">{errors.password.message}</small>
-          )}
-          <div className="input-group">
-            <label>Nome Completo</label>
-            <input
-              className="default-input"
-              type="text"
-              {...register("name", { required: true })}
-            />
-          </div>
-          {errors.name && (
-            <small className="text-red-600">{errors.name.message}</small>
-          )}
-          <div className="input-group">
-            <label>E-mail</label>
-            <input
-              className="default-input"
-              type="email"
-              {...register("email", { required: true })}
-            />
-          </div>
-          {errors.email && (
-            <small className="text-red-600">{errors.email.message}</small>
-          )}
-          <div className="input-group">
-            <label>Endereço</label>
-            <input
-              className="default-input"
-              type="text"
-              {...register("address", { required: true })}
-            />
-          </div>
-          {errors.address && (
-            <small className="text-red-600">{errors.address.message}</small>
-          )}
-          <div className="input-group">
-            <label>Telefone</label>
-            <input
-              className="default-input"
-              type="tel"
-              maxLength={15}
-              onInput={(e) => setPhoneValue(handlePhoneChange(e))}
-              value={phoneValue}
-              {...register("phone", { required: true })}
-            />
-          </div>
-          {errors.phone && (
-            <small className="text-red-600">{errors.phone.message}</small>
-          )}
-          <div className="input-group">
-            <label>CNPJ</label>
-            <input
-              className="default-input"
-              type="text"
-              onInput={(e) => setCnpjValue(handleCnpjChange(e))}
-              value={cnpjValue}
-              maxLength={18}
-              {...register("cnpj", { required: true })}
-            />
-          </div>
-          {errors.cnpj && (
-            <small className="text-red-600">{errors.cnpj.message}</small>
-          )}
-          <div className="input-group">
-            <label>Tipo</label>
-            <select
-              className="default-input"
-              {...register("type", { required: true })}
-              value={"cliente"}
-            >
-              <option value="cliente">Microempreendedor</option>
-              <option value="fornecedor">Fornecedor</option>
-            </select>
-          </div>
-          {errors.type && (
-            <small className="text-red-600">{errors.type.message}</small>
-          )}
-        </div>
-        <div className="flex justify-between">
-          <Link href={"/"}>
-            <button className="alt-button self-start mt-8">Voltar</button>
-          </Link>
-          <button className="self-start mt-8">Enviar</button>
-        </div>
-      </form>
+        </form>
+      )}
       <Modal
         isOpen={isModalOpen}
         closeMessage={"Confirmar"}
@@ -205,7 +224,7 @@ export default function Signup() {
           redirect("/");
         }}
       >
-        Parabéns, sua conta foi criada com sucesso!!
+        {isNotActive? "Houve um erro com CNJP, por favor tente novamente" : "Parabéns, sua conta foi criada com sucesso!!"}
       </Modal>
     </div>
   );
